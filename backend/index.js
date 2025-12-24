08949141f36eec9519bff768d846802caed7d57e
Varshit Tyagi <vksingh1122001@gmail.com>
2025-12-25 00:09:07 +0530

    imporoved scripts

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV_CUSTOM === "production") {
   });
 }
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
